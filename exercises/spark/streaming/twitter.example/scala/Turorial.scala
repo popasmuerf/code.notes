7727b00e31d4578ea53c79858bc1a450e2b41659
Git Test <MichaelBrown172@Gmail.com>
2016-08-23 15:21:19 -0400

    .....

---
+++ exercises/spark/streaming/twitter.example/scala/Turorial.scala
@@ -0,0 +1,12 @@
+import spark._ 
+import spark.streaming._
+import StreamingContext._
+import TutorialHelper._
+
+
+object Tutorial{
+    def main(args:Array[String]){
+        val sparkHome = "
+    }
+
+}
